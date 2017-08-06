e2b1dbbdd391efad894a1f61221fa6e36fe924eb
eduard <montylabru@gmail.com>
2017-08-06 23:56:06 +0300

    input element

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,45 +1,56 @@
 <template>
-    <div class="hello">
-        <app-calendar-widget></app-calendar-widget>
-        <!--<app-income-widget></app-income-widget>-->
-        <!--<app-outcome-widget></app-outcome-widget>-->
-        <app-datasheet-widget title="Income" :service="incomeService"></app-datasheet-widget>
-        <app-datasheet-widget title="Outcome" :service="outcomeService"></app-datasheet-widget>
-    </div>
+  <div class="hello">
+    <app-calendar-widget></app-calendar-widget>
+
+    <app-datasheet-input
+      :categories="categories"
+      :category="category"
+      :description="description"
+    ></app-datasheet-input>
+
+    <app-datasheet-widget title="Income" :service="incomeService"></app-datasheet-widget>
+    <app-datasheet-widget title="Outcome" :service="outcomeService"></app-datasheet-widget>
+  </div>
 </template>
 
 <script>
   import incomeService from '@/services/income-service'
   import outcomeService from '@/services/outcome-service'
+  import AppDatasheetInput from "./datasheet/input.vue";
 
-	export default {
-		name: 'dashboard',
-		data () {
-			return {
-				incomeService,
-				outcomeService
-            }
-		}
-	}
+  export default {
+    components: {AppDatasheetInput},
+    name: 'dashboard',
+    data() {
+      return {
+        incomeService,
+        outcomeService,
+
+        categories: outcomeService.categories,
+        category: 'category:subcategory',
+        description: 'description'
+      }
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
+  h1, h2 {
+    font-weight: normal;
+  }
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-    a {
-        color: #42b983;
-    }
+  a {
+    color: #42b983;
+  }
 </style>
