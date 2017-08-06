105ff0241ef71ee0e8bae00a09d82b29c8fe7db7
eduard <montylabru@gmail.com>
2017-08-06 18:25:09 +0300

    input

---
+++ src/components/datasheet/input.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'app-datasheet-input',
+  props: ['categories', 'description', 'category'],
+
+}
+</script>
+
+<template>
+  <div class="categoryInput" @onkeydown="change">
+    <span class="category"></span>
+    <span class="description"></span>
+  </div>
+</template>
+
+<style scoped>
+  .category {
+    color: red;
+  }
+
+  .description {
+    ca
+  }
+</style>
